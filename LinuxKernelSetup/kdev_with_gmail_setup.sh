77b128d995e052bfb837ae3c11b0a1a905984734
Benjamin Young <youngcdev@gmail.com>
2015-12-10 13:20:55 -0500

    Added sublime script start up script "subl" for /usr/local/bin Updated kernel dev script with new mutt configuration and package installation

--- LinuxKernelSetup/kdev_with_gmail_setup.sh
+++ LinuxKernelSetup/kdev_with_gmail_setup.sh
@@ -12,34 +12,24 @@ read GMAIL_PASSWORD
 MUTTRC_FILE=~/.muttrc
 
 cat > $MUTTRC_FILE <<- EOM
-# A basic .muttrc for use with Gmail
-# Change the following six lines to match your Gmail account details
-set imap_user = "$GMAIL_USER@gmail.com"
-set imap_pass = "$GMAIL_PASSWORD"
-set smtp_url = "smtp://$GMAIL_USER@smtp.gmail.com:587"
-set smtp_pass = "$GMAIL_PASSWORD"
-set from = "$GMAIL_USER@gmail.com"
-set realname="$NAME"
-# Change the following line to a different editor you prefer.
+# Folder hooks
+folder-hook 'account.com.gmail.$GMAIL_USER' 'source ~/.mutt/account.com.gmail.$GMAIL_USER'
+
+# Default account
+source ~/.mutt/account.com.gmail.$GMAIL_USER
+
+# Macros for switching accounts
+
+macro index <F12> '<sync-mailbox><enter-command>source ~/.mutt/account.com.gmail.$GMAIL_USER<enter><change-folder>!<enter>'
+
+# Set default text editor
 set editor = "nano"
-# Basic config, you can leave this as is
-set ssl_starttls = yes
-set ssl_force_tls = yes
-set header_cache="~/.mutt/cache/headers"
-set message_cachedir="~/.mutt/cache/bodies"
-set certificate_file="~/.mutt/certificates"
-set folder = "imaps://imap.gmail.com:993"
-set spoolfile = "+INBOX"
+
+#-------- Basic Config ------------
 set imap_check_subscribed
-set hostname = gmail.com
 set mail_check = 120
 set timeout = 300
 set imap_keepalive = 300
-set postponed = "+[GMail]/Drafts"
-set record = "+[GMail]/Sent Mail"
-set header_cache="~/.mutt/cache/headers"
-set message_cachedir="~/.mutt/cache/bodies"
-set certificate_file="~/.mutt/certificates"
 set move = no
 set include
 set sort = 'threads'
@@ -53,16 +43,31 @@ alternative_order text/plain text/html *
 auto_view text/html
 bind editor <Tab> complete-query
 bind editor ^T complete
-bind editor <space> noop
-# Gmail-style keyboard shortcuts
-macro index,pager y "unset trash\n " "Gmail archive message"
-macro index,pager d "set trash=\"imaps://imap.googlemail.com/[GMail]/Bin\"\n " "Gmail delete message"
-macro index,pager gi "=INBOX" "Go to inbox"
-macro index,pager ga "=[Gmail]/All Mail" "Go to all mail"
-macro index,pager gs "=[Gmail]/Starred" "Go to starred messages"
-macro index,pager gd "=[Gmail]/Drafts" "Go to drafts"
+bind editor <space> noop 
+
 EOM
 
+if [ ! -d ~/.mutt ]; then
+	mkdir ~/.mutt
+fi
+
+# Save MUTT Gmail Config
+GMAIL_MUTT_FILE=~/.mutt/account.com.gmail.$GMAIL_USER
+
+cat > $GMAIL_MUTT_FILE <<- EOM
+set imap_user = "$GMAIL_USER@gmail.com"                       
+set imap_pass = "$GMAIL_PASSWORD"                                    
+set smtp_url = "smtp://$GMAIL_USER@smtp.gmail.com:587/"       
+set smtp_pass = "$GMAIL_PASSWORD"                                    
+set from = "$GMAIL_USER@gmail.com"                            
+set realname = "$NAME"                                  
+set folder = "imaps://imap.gmail.com:993"                     
+set spoolfile = "+INBOX"                                      
+set postponed = "+[Gmail]/Drafts"                             
+set header_cache = ~/.mutt/com.gmail.$GMAIL_USER/cache/headers            
+set message_cachedir = ~/.mutt/com.gmail.$GMAIL_USER/cache/bodies         
+set certificate_file = ~/.mutt/com.gmail.$GMAIL_USER/certificates
+EOM
 
 # Save git configuration
 git config --global sendemail.smtpencryption tls
@@ -73,3 +78,6 @@ git config --global sendemail.smtppass $GMAIL_PASSWORD
 git config --global user.name "$NAME"
 git config --global user.email $GMAIL_USER@gmail.com
 git config --global core.editor nano
+
+# Add download packages with DNF for fedora
+sudo dnf install make gcc kernel-devel openssl-devel mutt nano
