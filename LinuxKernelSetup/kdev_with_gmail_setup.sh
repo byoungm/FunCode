d132f768ccf62e59229dfc07c3d96a17ba19f5c0
Benjamin Young <youngcdev@gmail.com>
2015-12-11 16:42:46 -0500

    Fixed git-send email
    Needed to add msmtp to fix the git-send-email becuase git-send email cound
    not authenciate without it.

--- LinuxKernelSetup/kdev_with_gmail_setup.sh
+++ LinuxKernelSetup/kdev_with_gmail_setup.sh
@@ -43,7 +43,7 @@ alternative_order text/plain text/html *
 auto_view text/html
 bind editor <Tab> complete-query
 bind editor ^T complete
-bind editor <space> noop 
+bind editor <space> noop
 
 EOM
 
@@ -55,29 +55,50 @@ fi
 GMAIL_MUTT_FILE=~/.mutt/account.com.gmail.$GMAIL_USER
 
 cat > $GMAIL_MUTT_FILE <<- EOM
-set imap_user = "$GMAIL_USER@gmail.com"                       
-set imap_pass = "$GMAIL_PASSWORD"                                    
-set smtp_url = "smtp://$GMAIL_USER@smtp.gmail.com:587/"       
-set smtp_pass = "$GMAIL_PASSWORD"                                    
-set from = "$GMAIL_USER@gmail.com"                            
-set realname = "$NAME"                                  
-set folder = "imaps://imap.gmail.com:993"                     
-set spoolfile = "+INBOX"                                      
-set postponed = "+[Gmail]/Drafts"                             
-set header_cache = ~/.mutt/com.gmail.$GMAIL_USER/cache/headers            
-set message_cachedir = ~/.mutt/com.gmail.$GMAIL_USER/cache/bodies         
+set imap_user = "$GMAIL_USER@gmail.com"
+set imap_pass = "$GMAIL_PASSWORD"
+set smtp_url = "smtp://$GMAIL_USER@smtp.gmail.com:587/"
+set smtp_pass = "$GMAIL_PASSWORD"
+set from = "$GMAIL_USER@gmail.com"
+set realname = "$NAME"
+set folder = "imaps://imap.gmail.com:993"
+set spoolfile = "+INBOX"
+set postponed = "+[Gmail]/Drafts"
+set header_cache = ~/.mutt/com.gmail.$GMAIL_USER/cache/headers
+set message_cachedir = ~/.mutt/com.gmail.$GMAIL_USER/cache/bodies
 set certificate_file = ~/.mutt/com.gmail.$GMAIL_USER/certificates
 EOM
 
 # Save git configuration
 git config --global sendemail.smtpencryption tls
-git config --global sendemail.smtpserver smtp.gmail.com
+git config --global sendemail.smtpserver /usr/bin/msmtp
 git config --global sendemail.smtpuser $GMAIL_USER.gmail.com
 git config --global sendemail.smtpserverport 587
-git config --global sendemail.smtppass $GMAIL_PASSWORD
 git config --global user.name "$NAME"
 git config --global user.email $GMAIL_USER@gmail.com
 git config --global core.editor nano
 
+MSMTP_CONFIG_FILE=~/.msmtprc
+cat > $MSMTP_CONFIG_FILE <<- EOM
+# Setup MSMTP for git send-email
+# Set default values for all following accounts.
+defaults
+tls on
+tls_trust_file /etc/pki/tls/certs/ca-bundle.crt
+logfile ~/.msmtp.log
+
+# My email service
+account gmail
+host smtp.gmail.com
+port 587
+from $GMAIL_USER@gmail.com
+auth on
+user $GMAIL_USER@gmail.com
+
+# Set a default account
+account default : gmail
+
+EOM
+
 # Add download packages with DNF for fedora
-sudo dnf install make gcc kernel-devel openssl-devel mutt nano
+sudo dnf install make gcc kernel-devel openssl-devel mutt nano git-email msmtp
